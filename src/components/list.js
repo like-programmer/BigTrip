7d53e9076a178a28a688c8a8f7e81e6670628020
Darya796 <dashutkadevi@gmail.com>
2020-04-21 13:39:47 +0300

    move create functions to components & add import/export

---
+++ src/components/list.js
@@ -0,0 +1,47 @@
+export const createListTemplate = () => {
+    return (`
+    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+                <span class="trip-sort__item  trip-sort__item--day">Day</span>
+
+                <div class="trip-sort__item  trip-sort__item--event">
+                    <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort"
+                           value="sort-event" checked>
+                    <label class="trip-sort__btn" for="sort-event">Event</label>
+                </div>
+
+                <div class="trip-sort__item  trip-sort__item--time">
+                    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort"
+                           value="sort-time">
+                    <label class="trip-sort__btn" for="sort-time">
+                        Time
+                        <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+                            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+                        </svg>
+                    </label>
+                </div>
+
+                <div class="trip-sort__item  trip-sort__item--price">
+                    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort"
+                           value="sort-price">
+                    <label class="trip-sort__btn" for="sort-price">
+                        Price
+                        <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+                            <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+                        </svg>
+                    </label>
+                </div>
+
+                <span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+            </form>
+            
+            <ul class="trip-days">
+            <li class="trip-days__item  day">
+    <div class="day__info">
+        <span class="day__counter">1</span>
+        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+    </div>
+
+    <ul class="trip-events__list"></ul>
+</ul>
+    `);
+};
