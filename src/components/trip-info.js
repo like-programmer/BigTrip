b5c2159b297f4479be4136ee5d26417cafb8e620
Darya796 <dashutkadevi@gmail.com>
2020-05-05 17:18:07 +0300

    3.3.1 move trip info data into the variables

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,14 +1,26 @@
+const createtripName = () => {
+  return (`Amsterdam &mdash; Chamonix &mdash; Geneva`);
+};
+
+const createtripLimits = () => {
+  return (`Mar 18&nbsp;&mdash;&nbsp;20`);
+};
+
 export const createTripInfoTemplate = () => {
-    return (`
+  const tripName = createtripName();
+  const tripLimits = createtripLimits();
+  const totalPrice = 1000;
+
+  return (`
     <section class="trip-main__trip-info  trip-info">
             <div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+              <h1 class="trip-info__title">${tripName}</h1>
 
-              <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+              <p class="trip-info__dates">${tripLimits}</p>
             </div>
 
             <p class="trip-info__cost">
-              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+              Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
             </p>
           </section>
 `);
