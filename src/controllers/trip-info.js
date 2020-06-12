fb6aa65aac38f5e978de755179cdc22f1dd94f41
like-programmer <dashutkadevi@gmail.com>
2020-06-12 14:54:32 +0300

    7.1.10 create a trip-info controller

---
+++ src/controllers/trip-info.js
@@ -0,0 +1,35 @@
+import TripInfoComponent from "../components/trip-info.js";
+
+import {RenderPosition, render, replace} from "../utils/render.js";
+
+export default class TripInfoController {
+  constructor(container, pointsModel) {
+    this._container = container;
+    this._pointsModel = pointsModel;
+
+    this._tripInfoComponent = null;
+
+    this._dataChangeHandler = this._dataChangeHandler.bind(this);
+
+    this._pointsModel.setDataChangeHandler(this._dataChangeHandler);
+  }
+
+  render() {
+    const container = this._container;
+    const allPoints = this._pointsModel.getPointsAll();
+
+    const oldComponent = this._tripInfoComponent;
+
+    this._tripInfoComponent = new TripInfoComponent(allPoints);
+
+    if (oldComponent) {
+      replace(this._tripInfoComponent, oldComponent);
+    } else {
+      render(container, this._tripInfoComponent, RenderPosition.AFTERBEGIN);
+    }
+  }
+
+  dataChangeHandler() {
+    this.render();
+  }
+}
